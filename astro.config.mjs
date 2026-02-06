ee1f2f2e8896b821bf7483f077d11b74b3ebd0cd
Avish Jha <avish.j@protonmail.com>
2026-02-06 02:05:57 -0500

    fix: handle dynamic paths for ci/local
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+const isProd = import.meta.env.PROD;
+
 // https://astro.build/config
 export default defineConfig({
+  site: isProd ? 'https://avishj.github.io' : 'http://localhost:4321',
+  base: isProd ? '/avishj.dev' : '/',
   vite: {
     plugins: [tailwindcss()],
   },

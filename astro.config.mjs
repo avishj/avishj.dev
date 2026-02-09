e6390614fe957f2bfc8ef5f8cd42826c3eabfbdb
Avish Jha <avish.j@protonmail.com>
2026-02-09 04:52:14 -0500

    deps: add astro mdx
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 const isProd = import.meta.env.PROD;
 
 // https://astro.build/config
@@ -9,7 +11,10 @@ export default defineConfig({
   site: isProd ? 'https://avishj.github.io' : 'http://localhost:4321',
   base: isProd ? '/avishj.dev' : '',
   trailingSlash: 'ignore',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx()],
 });
